cdea43171d3fc25b334fa6c8b939426d3b1c06f7
Konstantin Babayan <k.babayan@yadro.com>
2024-10-21 19:48:54 +0300

    Try to complite lab4, but fail

--- my_labs/lab4/include/iomanip.hpp
+++ my_labs/lab4/include/iomanip.hpp
@@ -8,49 +8,62 @@
 #define __IOMANIP_HPP__
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
-// Манипулятор endm: добавляет новую строку и очищает поток
+// Манипулятор `endm` добавляет "[eol]\n" к потоку
 inline std::ostream& endm(std::ostream& os) {
-    os.put('\n');  // Добавляем новую строку
-    os.flush();    // Очищаем буфер вывода
+    os << "[eol]\n";
     return os;
 }
 
-// Манипулятор squares: выводит квадрат числа
-class squares_manip {
+// Манипулятор `squares` выводит значение в квадратных скобках
+struct squares_t {};
+inline squares_t squares;
+
+// Перегрузка оператора для манипулятора squares
+inline std::ostream& operator<<(std::ostream& os, const squares_t&) {
+    // Не выводим ничего, просто возвращаем поток
+    return os;
+}
+
+// Оператор для закрытия квадратных скобок
+inline std::ostream& operator<<(std::ostream& os, const squares_t&) {
+    os << "]"; // Если нужно, можно добавить логику для вывода, если это необходимо
+    return os;
+}
+
+// Класс для обработки сложения значений
+class add_t {
 public:
-    int value;
-    squares_manip(int v) : value(v) {}
+    add_t() : sum_(0.0) {}
+
+    // Перегрузка оператора для добавления значений
+    template<typename T>
+    add_t& operator<<(const T& value) {
+        std::stringstream ss;
+        ss << value;
+
+        double number = 0.0;
+        if (!(ss >> number)) {
+            std::cerr << "Invalid argument: " << value << " cannot be converted to double." << std::endl;
+            return *this; // Возвращаем текущее состояние без изменения суммы
+        }
+
+        sum_ += number; // Добавляем к сумме
+        return *this;
+    }
+
+    // Перегрузка оператора вывода для отображения суммы
+    friend std::ostream& operator<<(std::ostream& os, const add_t& add) {
+        os << add.sum_;
+        return os;
+    }
+
+private:
+    double sum_;
 };
 
-// Перегрузка оператора << для манипулятора squares
-inline std::ostream& operator<<(std::ostream& os, const squares_manip& sq) {
-    return os << sq.value * sq.value;  // Выводим квадрат числа
-}
-
-// Функция для использования манипулятора squares
-inline squares_manip squares(int value) {
-    return squares_manip(value);  // Возвращаем объект squares_manip
-}
-
-// Манипулятор add: складывает два числа
-template <typename T>
-class add_manip {
-public:
-    T a, b;
-    add_manip(T _a, T _b) : a(_a), b(_b) {}
-};
-
-// Перегрузка оператора << для манипулятора add
-template <typename T>
-inline std::ostream& operator<<(std::ostream& os, const add_manip<T>& add) {
-    return os << (add.a + add.b);  // Выводим сумму двух чисел
-}
-
-// Функция для использования манипулятора add
-template <typename T>
-inline add_manip<T> add(T a, T b) {
-    return add_manip<T>(a, b);  // Возвращаем объект add_manip
-}
+inline add_t add;
 
 #endif // __IOMANIP_HPP__
